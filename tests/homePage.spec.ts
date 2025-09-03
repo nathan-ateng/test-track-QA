e3443677f38e849977382838833309cb3d481bc2
Nathan Ateng <nathanateng@gmail.com>
2025-09-03 22:45:51 +0300

    Add: Test To Verify Homepage Texts

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -22,3 +22,17 @@ test("Verify Live clock is visible and accurate", async ({ page }) => {
   // Assert that the live clock element is visible and contains the correct time.
   await expect(liveClockElement).toHaveText(expectedTimeRegex);
 });
+
+test("Verify Pressence Header Texts ", async ({ page }) => {
+  // Console Text
+  const consoleText = page.locator(".text-primary.text-sm");
+  await expect(consoleText).toContainText("CONSOLE 001 READY");
+
+  // Test Track Header Text
+  const headerText = page.locator("h1[class='text-4xl sm:text-5xl lg:text-7xl font-black text-primary font-futura tracking-[0.15em] drop-shadow-lg mb-2']");
+  await expect(headerText).toHaveText("TEST TRACK");
+
+  //Tagline Text
+  const taglineText = page.locator("div[class='text-xs sm:text-sm text-accent tracking-[0.2em] mb-4 font-futura']");
+  await expect(taglineText).toHaveText("AUTONOMOUS AGENT TRAINING FACILITY");
+});
